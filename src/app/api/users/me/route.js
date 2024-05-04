61125602f551c182e79d43006ac4c0bfbe4ef8db
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-05-04 21:43:46 +0500

    collecting data from jwt token

---
+++ src/app/api/users/me/route.js
@@ -0,0 +1,16 @@
+import { dataFromToken } from "@/helpers/dataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+
+connect();
+
+export async function GET(request) {
+  try {
+    const userId = await dataFromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password");
+    return NextResponse.json({ message: "User found", data: user });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
